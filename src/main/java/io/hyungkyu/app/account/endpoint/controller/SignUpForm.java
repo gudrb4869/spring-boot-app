c4537701ab3bb4df8fe71635c9a8e956919ff8b3
HyungKyuPark <gudrb4869@naver.com>
2022-09-28 20:10:17 +0900

    #3

---
+++ src/main/java/io/hyungkyu/app/account/endpoint/controller/SignUpForm.java
@@ -0,0 +1,10 @@
+package io.hyungkyu.app.account.endpoint.controller;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+    private String nickname;
+    private String email;
+    private String password;
+}
