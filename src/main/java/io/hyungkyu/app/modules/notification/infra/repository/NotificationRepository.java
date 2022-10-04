f84a782646313c670178c1dafcacc031253dc9ec
HyungKyuPark <gudrb4869@naver.com>
2022-10-04 18:45:09 +0900

    #63

--- src/main/java/io/hyungkyu/app/modules/notification/infra/repository/NotificationRepository.java
+++ src/main/java/io/hyungkyu/app/modules/notification/infra/repository/NotificationRepository.java
@@ -1,7 +1,9 @@
 package io.hyungkyu.app.modules.notification.infra.repository;
 
+import io.hyungkyu.app.modules.account.domain.entity.Account;
 import io.hyungkyu.app.modules.notification.domain.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    long countByAccountAndChecked(Account account, boolean checked);
 }
