d39131d6dc039c2dae2cf5316022607a14b3114a
HyungKyuPark <gudrb4869@naver.com>
2022-10-04 21:40:03 +0900

    #69

--- src/main/java/io/hyungkyu/app/modules/study/infra/repository/StudyRepositoryExtensionImpl.java
+++ src/main/java/io/hyungkyu/app/modules/study/infra/repository/StudyRepositoryExtensionImpl.java
@@ -1,14 +1,16 @@
 package io.hyungkyu.app.modules.study.infra.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.JPQLQuery;
 import io.hyungkyu.app.modules.account.domain.entity.QAccount;
 import io.hyungkyu.app.modules.study.domain.entity.QStudy;
 import io.hyungkyu.app.modules.study.domain.entity.Study;
 import io.hyungkyu.app.modules.tag.domain.entity.QTag;
 import io.hyungkyu.app.modules.zone.domain.entity.QZone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-
-import java.util.List;
 
 public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport implements StudyRepositoryExtension {
 
@@ -17,7 +19,7 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
     }
 
     @Override
-    public List<Study> findByKeyword(String keyword) {
+    public Page<Study> findByKeyword(String keyword, Pageable pageable) {
         QStudy study = QStudy.study;
         JPQLQuery<Study> query = from(study)
                 .where(study.published.isTrue()
@@ -28,6 +30,15 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
                 .leftJoin(study.zones, QZone.zone).fetchJoin() // zones join 및 fetchJoin
                 .leftJoin(study.members, QAccount.account).fetchJoin()
                 .distinct(); // members join 및 fetchJoin
-        return query.fetch();
+        JPQLQuery<Study> pageableQuery = getQuerydsl().applyPagination(pageable, query);
+        QueryResults<Study> fetchResults = pageableQuery.fetchResults();
+        return new PageImpl<>(fetchResults.getResults(), pageable, fetchResults.getTotal());
     }
+    /**
+     * getQuerydsl을 이용해 QuerydslRepositorySupport 가 제공하는 기능을 사용할 수 있는데 페이징을 적용하기 위해 applyPagination을 호출함.
+     * fetchResults 를 이용해 조회한 결과를 얻을  수 있음.
+     * 반환해야 할 타입이 Page 이므로 구현체인 PageImpl 을 이용해 반환함.
+     * 결과 데이터, pageable, 전체 데이터 수를 생성자로 전달해주어야 함.
+     * 마지막으로 MainController 에서 view 로 전달해주는 이름이 바뀌었기 때문에 search.html 파일도 수정해주어야 함.
+     */
 }
